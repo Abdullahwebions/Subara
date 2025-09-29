b33ff4349adcce5d0dbcd3309491d0e69d794172
Abdullahwebions <131215945+Abdullahwebions@users.noreply.github.com>
2025-09-29 11:32:43 +0300

    Update swaper.js

--- Js/swaper.js
+++ Js/swaper.js
@@ -1,18 +1,1 @@
-new Swiper('.SwiperContainer', {
-  // loop: true,
 
-  breakpoints: {
-    0:{
-      slidesPerView:2,
-      spaceBetween: 10
-    },
-    768:{
-      slidesPerView:3,
-      spaceBetween:20
-    },
-    1024:{
-      slidesPerView:5,
-      spaceBetween:25
-    },
-  }
-});
